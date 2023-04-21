e3b3cac79d61a032f0f2796e2d484661856affc3
alejandropmz <alejandropacheco2497@gmail.com>
2023-04-20 23:41:16 -0500

    navbar and search input

--- app/app.py
+++ app/app.py
@@ -1,11 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for
 
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("layout.html")
+    return redirect(url_for("pacientes"))
+
+@app.route("/pacientes")
+def pacientes():
+    return render_template("patients.html")
 
 if __name__ == "__main__":
     app.run(debug=True, port=4000)
