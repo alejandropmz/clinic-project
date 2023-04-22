31d1420952a6db4ca3d80678dd95d85d74a143a8
alejandropmz <alejandropacheco2497@gmail.com>
2023-04-21 19:22:20 -0500

    forms styles and functional dropdown buttons

--- app/app.py
+++ app/app.py
@@ -1,15 +1,39 @@
 from flask import Flask, render_template, redirect, url_for
+from flask_mysqldb import MySQL
 
 
 app = Flask(__name__)
+
+mysql = MySQL(app)
+
+app.config["MYSQL_HOST"] = "localhost"
+app.config["MYSQL_USER"] = "root"
+app.config["MYSQL_PASSWORD"] = "123456"
+app.config["MYSQL_DB"] = "clinic"
+
 
 @app.route("/")
 def index():
     return redirect(url_for("pacientes"))
 
+
 @app.route("/pacientes")
 def pacientes():
-    return render_template("patients.html")
+    cursor = mysql.connection.cursor()
+    cursor.execute("SELECT * FROM pacientes")
+    patients = cursor.fetchall()
+    cursor.close()
+
+    # appointments
+    cursor = mysql.connection.cursor()
+    cursor.execute("SELECT * FROM citas")
+    appointments = cursor.fetchall()
+    cursor.close()
+    print(appointments)
+    return render_template(
+        "patients.html", patients=patients, appointments=appointments
+    )
+
 
 if __name__ == "__main__":
     app.run(debug=True, port=4000)
