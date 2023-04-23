b2c20d8e576443573fbd33aa0f87a6c5f3078879
alejandropmz <alejandropacheco2497@gmail.com>
2023-04-23 17:38:19 -0500

    bill prices interactivity

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -18,3 +18,28 @@ document.addEventListener("DOMContentLoaded", function () {
     }
   });
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+  const costInput = document.getElementById("cost-input");
+  const costOutput = document.getElementById("cost-output");
+  const constIvaOutput = document.getElementById("const-iva-output");
+  const totalCost = document.getElementById("total-cost");
+
+  costInput.addEventListener("input", function (e) {
+    if (e.target.value === "") {
+      costOutput.value = "$" + 0;
+      constIvaOutput.value = "$" + 0;
+      totalCost.value = "$" + 0;
+    } else {
+      costOutput.value = "$" + parseInt(e.target.value).toLocaleString();
+      constIvaOutput.value =
+        "$" + (parseInt(e.target.value) * 0.19).toLocaleString();
+      totalCost.value =
+        "$" +
+        (
+          parseInt(e.target.value) +
+          parseInt(e.target.value) * 0.19
+        ).toLocaleString();
+    }
+  });
+});
